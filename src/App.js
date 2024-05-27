cee95371ceafa74b08fb48e8eea513dc90f7c266
Assaf <1214assaf@gmail.com>
2024-05-27 10:35:13 +0300

    refactor: Implement nested routing in App.js and add RootLayout component

--- src/App.js
+++ src/App.js
@@ -26,13 +26,20 @@ import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root.js";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/:id", element: <EventDetailPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/events/:id/edit", element: <EditEventPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:id", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:id/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 function App() {
