f183db493f915693c7260ff813079b9f86065a6b
Assaf <1214assaf@gmail.com>
2024-05-27 14:54:28 +0300

    refactor: Update NewEventPage to use EventForm component and add newEventAction

--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -1,8 +1,30 @@
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return (
-    <div>
-      <h1>New Event Page</h1>
-    </div>
-  );
+  return <EventForm />;
 }
 export default NewEventPage;
+export async function action({ request }) {
+  const data = request.formData();
+  const eventData = {
+    title: data.get("title"),
+    description: data.get("description"),
+    date: data.get("date"),
+    image: data.get("image"),
+  };
+
+  const response = await fetch("http://localhost:3000/api/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not create the event" }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
